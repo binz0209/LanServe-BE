ab1f63325de483fee4fdf12aa598daed2c85cdf0
Huu Hoai Linh Nguyen <hoailinh.dienban@gmail.com>
2025-09-28 17:31:28 +0700

    [LinhNHH] Add Health

---
+++ LanServe.Api/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using LanServe.Infrastructure.Data;
+
+namespace LanServe.Api.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get([FromServices] IOptions<MongoOptions> opt)
+    {
+        return Ok(new
+        {
+            ok = true,
+            dbConfigured = !string.IsNullOrWhiteSpace(opt.Value.ConnectionString) && !string.IsNullOrWhiteSpace(opt.Value.DbName),
+            db = opt.Value.DbName
+        });
+    }
+}
